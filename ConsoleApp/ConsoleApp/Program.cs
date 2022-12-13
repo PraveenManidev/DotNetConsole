5bb6ba745910a5e50f77fb334608ee5a299b27e3
PraveenManidev <praveenthuyamani@gmail.com>
2022-12-14 01:35:35 +0530

    console app

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
